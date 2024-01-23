aba1e7562bc53a7b284150b9d9a7eabb988a69f1
Martin R. Pfaller <pfaller@stanford.edu>
2024-01-23 14:37:13 -0800

    change filename

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import os
 FDIR = os.path.dirname(__file__)
 
 # define paths
-f_geometries = os.path.join(FDIR, "geometries_vmr_richter22.txt")
+f_geometries = os.path.join(FDIR, "geometries_vmr_pfaller22.txt")
 f_geo_in = os.path.join(FDIR, "data", "geometric_pfaller22", "input")
 f_cali_0d_in = os.path.join(FDIR, "data", "calibrated_richter24_from_0d", "input")
 f_cali_0d_out = os.path.join(FDIR, "data", "calibrated_richter24_from_0d", "output")
