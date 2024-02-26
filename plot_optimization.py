c500de0b2a945b43e6ad3cb97ecea0d87002d51d
Martin R. Pfaller <pfaller@stanford.edu>
2024-02-25 21:36:44 -0800

    Horizontal correlation plots

--- plot_optimization.py
+++ plot_optimization.py
@@ -11,6 +11,7 @@ from collections import defaultdict
 
 from utils import (
     f_out,
+    f_picture,
     f_geo_in,
     f_cali_0d_out,
     f_cali_3d_out,
@@ -22,6 +23,33 @@ from utils import (
 plt.rcParams.update(
     {"text.usetex": True, "font.family": "serif", "font.serif": "Computer Modern Roman"}
 )
+
+
+def add_img(ax, geo):
+    # load image
+    impath = os.path.join(f_picture, geo + '.png')
+    img = plt.imread(impath)
+
+    # Determine whether to scale based on width or height
+    aspect_ratio = img.shape[1] / img.shape[0]
+    if aspect_ratio > 1:
+        target_width = 1
+        target_height = target_width / aspect_ratio
+    else:
+        target_height = 1
+        target_width = target_height * aspect_ratio
+
+    # Calculate the center of the target box
+    center_x = (-3 + -2) / 2
+    center_y = (0 + 1) / 2
+
+    # Adjust the extent based on the calculated center and target size
+    left = center_x - target_width / 2
+    right = center_x + target_width / 2
+    bottom = center_y - target_height / 2
+    top = center_y + target_height / 2
+
+    ax.imshow(img, extent=[left, right, bottom, top], aspect='auto')
 
 
 def plot(dim):
@@ -36,21 +64,21 @@ def plot(dim):
     files, cats = get_geometries()
 
     # compare 0D element values
-    nx = 9
-    ny = 8
+    nv = 18
+    nh = 4
 
     # set element properties
-    elements = ["R_poiseuille", "stenosis_coefficient", "L", "C"]
+    elements = ["R_poiseuille", "L", "C", "stenosis_coefficient"]
     colors = {}
     for i, e in enumerate(elements):
         colors[e[0]] = plt.cm.Dark2(i)
-    elements_pos = [[-1, 0], [0, 0], [-1, -1], [0, -1]]
+    elements_pos = np.array([[-2, 0], [-1, 0], [0, 0], [1, 0]])
 
-    fig, ax = plt.subplots(nx, ny, figsize=(ny * 2, nx * 2.5), dpi=300)
+    fig, ax = plt.subplots(nv, nh, figsize=(nh * 3, nv), dpi=500)
 
     correlations = defaultdict(list)
     for j, (fname, cat) in enumerate(zip(files, cats)):
-        ab = np.unravel_index(j, (nx, ny))
+        ab = np.unravel_index(j, (nv, nh))
 
         # read results
         with open(os.path.join(f_geo_in, fname + ".json")) as f:
@@ -72,8 +100,8 @@ def plot(dim):
             sol = np.array(sol)
 
             # get some statistics
-            slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(ref, sol)
-            correlations[ele] += [r_value ** 2]
+            _, _, r_value, _, _ = scipy.stats.linregress(ref, sol)
+            correlations[ele] += [r_value**2]
 
             # manually set limits
             xlim = [ref.min(), ref.max()]
@@ -92,50 +120,45 @@ def plot(dim):
 
             # put plot in the correct quadrant
             offset = elements_pos[j]
-
             ax[ab].scatter(ref + offset[0], sol + offset[1], s=50, color=colors[ele[0]])
-            ax[ab].set_xticklabels([])
-            ax[ab].set_yticklabels([])
-            ax[ab].spines["top"].set_visible(True)
-            ax[ab].spines["right"].set_visible(True)
-            ax[ab].tick_params(
-                left=False, bottom=False, labelleft=False, labelbottom=False
-            )
 
             # plot diagonal
             diag = np.array([0, 1])
             ax[ab].plot(diag + offset[0], diag + offset[1], "k--", linewidth=1.5)
 
+        # add model image
+        add_img(ax[ab], fname)
+
         # plot dividers
-        ax[ab].plot([-1, 1], [0, 0], "k-")
-        ax[ab].plot([0, 0], [-1, 1], "k-")
+        for pos in elements_pos:
+            px = [pos[0], pos[0]]
+            py = [pos[1], pos[1] + 1.0]
+            ax[ab].plot(px, py, "k-", linewidth=1.5)
 
         ax[ab].set_aspect("equal", adjustable="box")
-
-        total_lim = [-1, 1]
-        ax[ab].set_xlim(total_lim)
-        ax[ab].set_ylim(total_lim)
-        if fname in models_special:
-            col = "r"
-            title = "$\\textbf{" + fname + "}$"
-        else:
-            col = "k"
-            title = fname
+        ax[ab].set_xticklabels([])
+        ax[ab].set_yticklabels([])
+        ax[ab].spines["top"].set_visible(True)
+        ax[ab].spines["right"].set_visible(True)
+        ax[ab].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+        ax[ab].set_xlim([-3, 2])
+        ax[ab].set_ylim([0, 1])
+        col = "k"
+        title = fname
+        # if fname in models_special:
+        #     col = "r"
+        #     title = "$\\textbf{" + fname + "}$"
         ax[ab].set_title(title, fontsize=21, color=col)
-
-        plt.subplots_adjust(
-            left=0.05, right=0.95, top=0.95, bottom=0.05, wspace=0.2, hspace=0.2
-        )
 
     # print correlations
     print(str(dim) + "D r_value (mean, std)")
     for k, v in correlations.items():
         print(k, np.mean(v), np.std(v))
-    
+
     xtext = "Geometric 0D elements"
     ytext = "Optimized 0D elements from " + str(dim) + "D results"
     fig.text(0.5, -0.01, xtext, ha="center", fontsize=24)
-    fig.text(-0.01, 0.5, ytext, va="center", fontsize=24, rotation="vertical")
+    fig.text(-0.02, 0.5, ytext, va="center", fontsize=24, rotation="vertical")
     plt.tight_layout()
     fout = os.path.join(f_out, "optimized_" + str(dim) + "d.png")
     fig.savefig(fout, bbox_inches="tight")
